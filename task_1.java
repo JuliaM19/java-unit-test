7ecd29090adaef11c270f10715b541407fe3865c
Yuliya Mamontova <MamontovaYV@yandex.ru>
2023-06-25 23:12:04 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,9 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	// Напиши код здесь
+    boolean expected = true;
+   
+ assertEquals("Ошибка пользователю меньше 18 лет", expected, isAdult);
+}
